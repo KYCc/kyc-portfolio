00118e54aa3bf4c2d1927e3a5f0897a7942fd7e5
Kamiel Ceuppens <kamiel.ceuppens@student.kdg.be>
2025-11-01 00:39:40 +0100

    tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import './App.css'
 
 function App() {
 
   return (
-    <>
-        <h1>UNDER CONSTRUCTION</h1>
-    </>
+    <div className="flex items-center justify-center min-h-screen">
+        <h1 className="text-5xl">UNDER CONSTRUCTION</h1>
+    </div>
   )
 }
 
