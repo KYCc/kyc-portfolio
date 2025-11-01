92e1c7b2d502f4e52fc0ef7c0eab69baaabbb77a
Kamiel Ceuppens <kamiel.ceuppens@student.kdg.be>
2025-11-01 01:14:17 +0100

    under construction

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,14 @@
 function App() {
 
   return (
-    <div className="flex items-center justify-center min-h-screen">
-        <h1 className="text-5xl">UNDER CONSTRUCTION</h1>
-    </div>
+      <div className="relative min-h-screen w-full">
+          {/*background*/}
+          <div className="absolute inset-0 security-tape"></div>
+          {/*content layer*/}
+          <div className="relative flex items-center justify-center min-h-screen">
+              <h1 className="text-5xl font-bold text-white">UNDER CONSTRUCTION</h1>
+          </div>
+      </div>
   )
 }
 
